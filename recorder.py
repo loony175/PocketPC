38fb99048c348de5eba0679b8774098a6e506698
loony175 <l1997y03w01@gmail.com>
2018-12-30 19:54:39 +0800

    recorder.py: Fix typo

--- recorder.py
+++ recorder.py
@@ -15,7 +15,7 @@ import sys
 import time
 from urllib import parse
 
-USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'
+USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 
 def live48(room_id,format):
     time.sleep(1)
