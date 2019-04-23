c310bd21fab3911c71e08c4f42e45cf83e52fa28
loony175 <l1997y03w01@gmail.com>
2019-04-24 03:30:24 +0800

    Update member_id.json

--- member_id.py
+++ member_id.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python3
 
-import arrow
 import json
 import operator
 import requests
+import uuid
+
+HEADERS={
+    'Content-Type':'application/json',
+    'User-Agent':'PocketFans201807/6.0.0 (iPhone; iOS 12.2; Scale/2.00)',
+    'appInfo':json.dumps({
+        'vendor':'apple',
+        'deviceId':str(uuid.uuid4()).upper(),
+        'appVersion':'6.0.0',
+        'appBuild':'190409',
+        'osVersion':'12.2.0',
+        'osType':'ios',
+        'deviceName':'iphone',
+        'os':'ios',
+    }),
+}
 
 def main():
-    tzinfo='Asia/Shanghai'
-    time_format='%Y-%m-%d %H:%M:%S'
-    json_={}
-    json_['memberInfoUtime']=arrow.get(2012,10,14,tzinfo=tzinfo).strftime(time_format)
-    now=arrow.now(tzinfo).strftime(time_format)
-    for key in ['functionUtime','groupUtime','memberPropertyUtime','musicAlbumUtime','musicUtime','periodUtime','talkUtime','teamUtime','urlUtime','videoTypeUtime','videoUtime']:
-        json_[key]=now
-    resp=requests.post('https://psync.48.cn/syncsystem/api/cache/v1/update/overview',headers={'Content-Type':'application/json','version':'5.3.2','os':'android'},json=json_).json()
-    data=sorted(resp['content']['memberInfo'],key=operator.itemgetter('member_id'))
-    members=[dict['real_name'] for dict in data]
+    resp=requests.post('https://pocketapi.48.cn/user/api/v1/client/update/group_team_star',headers=HEADERS,json={}).json()
+    data=sorted(resp['content']['starInfo'],key=operator.itemgetter('userId'))
+    members=[dict['realName'] for dict in data]
     duplicate_names=[]
     for member in set(members):
         if members.count(member)>1:
             duplicate_names.append(member)
     members={}
     for dict in data:
-        member_name=dict['nick_name'] if dict['real_name'] in duplicate_names else dict['real_name']
-        members[member_name]=dict['member_id']
+        member_name=dict['nickname'] if dict['realName'] in duplicate_names else dict['realName']
+        members[member_name]=dict['userId']
     f=open('member_id.json','w')
     f.write(json.dumps(members,indent=2,ensure_ascii=False))
     f.write('\n')
